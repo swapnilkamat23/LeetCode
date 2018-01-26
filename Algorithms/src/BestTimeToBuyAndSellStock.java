26234678921a1100639bf6606961c42131def3c4
Swapnil Kamat <swapnilkamat23@gmail.com>
2018-01-25 23:17:07 -0500

    no message

---
+++ Algorithms/src/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,34 @@
+/**
+ * Author: Swapnil Kamat
+ */
+
+import java.util.*;
+
+public class BestTimeToBuyAndSellStock {
+	public static void main(String[] args){
+		List<Integer> prices = new LinkedList<Integer>();
+		prices.add(9);
+		prices.add(1);
+		prices.add(3);
+		prices.add(10);
+		prices.add(6);
+		System.out.println("profit= "+maxProfit(prices));
+	}
+	
+	public static int maxProfit(List<Integer> prices) {
+        if(prices.size()==0)
+            return 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[]  ans = new int[prices.size()];
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices.get(i) < min)
+                min = prices.get(i);
+            ans[i] = prices.get(i) - min;
+            if(ans[i]>max)
+                max = ans[i];
+        }
+        return max;
+    }
+}
