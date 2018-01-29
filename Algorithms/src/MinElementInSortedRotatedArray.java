ae63f0428b129cbb0a62b3c6b432dfd3370e88e9
Swapnil Kamat <swapnilkamat23@gmail.com>
2018-01-28 19:29:34 -0500

    no message

--- Algorithms/src/MinElementInSortedRotatedArray.java
+++ Algorithms/src/MinElementInSortedRotatedArray.java
@@ -2,31 +2,27 @@
 public class MinElementInSortedRotatedArray {
 
 	public int minElement(int[] arr){
-		if(arr.length == 1){
+		if(arr.length == 1)
 			return arr[0];
+		else{
+			return binarySearch(arr, 0, arr.length-1);
 		}
-		else if(arr[0]<arr[arr.length-1]){
-			return arr[0];
-		}
-		int result = binarySearch(arr, 0, arr.length-1);
-		return result;
 	}
 	
 	public int binarySearch(int[] arr, int start, int end){
-		if(start < end){
-			int mid = (start + end)/2;
-			if(mid-1>=0 && mid+1<arr.length){
-				if(arr[mid+1]<arr[mid]){
-					return arr[mid+1];
-				}
-				else if(arr[mid-1]>arr[mid]){
-					return arr[mid];
-				}
-				else if(arr[])
-			}
+		if(start == end){
+			return arr[start];
 		}
-		
-		return arr[start];
+		else if(start <= end){
+			
+		}
+		return -1;
+	}
+	
+	public static void main(String args[]){
+		MinElementInSortedRotatedArray obj = new MinElementInSortedRotatedArray();
+		int[] arr = new int[]{7,8,1,2,3,4,5,6};
+		System.out.println("Minimum element = " + obj.minElement(arr));
 	}
 	
 }
